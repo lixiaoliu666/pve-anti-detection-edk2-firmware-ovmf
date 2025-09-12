50aad2319947938705332508ec41964c6bbd91af
July's <37677092+july929@users.noreply.github.com>
2025-09-13 07:56:02 +0800

    Update build.sh

--- build.sh
+++ build.sh
@@ -12,7 +12,7 @@ cd pve-edk2-firmware
 apt install devscripts -y
 mk-build-deps --install
 git submodule update --init --recursive
-cp ../Logo.bmp debian/
+#cp ../Logo.bmp debian/
 cp ../sedPatch-pve-edk2-firmware-anti-dection.sh edk2/
 cd edk2
 meson subprojects download
